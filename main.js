8c9097d2c1340cf98a2b506206af3eed3e44c741
Codemagica0411 <83286689+Codemagica0411@users.noreply.github.com>
2023-03-23 17:51:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+function showinfo() {
+    document.getElementById("info").innerHTML = 'Some info: ' + knowledge;
+}
+
+var knowledge = "A lunar eclipse happens when the earth passes in between the moon and the sun. During a solar eclipse, the moon partially or fully hides the sun's rays for a few minutes. During a lunar eclipse, the earth's shade partially or fully hides the moon for a brief period of time"
